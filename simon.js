f5f572149f617f061ca551cd2f91ab808eb5b2a5
pratiksha950 <salunkepratiksha950@gmail.com>
2025-07-31 22:45:26 +0530

    keypress event applied

---
+++ simon.js
@@ -0,0 +1,12 @@
+let gameSequence=[];
+let UserSequence=[];
+
+let started=false;
+let level=0;
+document.addEventListener("keypress",function(){
+    if(started==false){
+        console.log("game started");
+        started=true;
+    }
+})
+
