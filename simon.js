ff78ea4ea48fdb94edf3c684ae4cb491d69dae69
pratiksha950 <salunkepratiksha950@gmail.com>
2025-08-01 06:16:54 +0530

    flash effect added

--- simon.js
+++ simon.js
@@ -1,12 +1,37 @@
 let gameSequence=[];
 let UserSequence=[];
+let btns=["yellow","red","purple","green"];
 
 let started=false;
 let level=0;
+let h2=document.querySelector("h2");
+
 document.addEventListener("keypress",function(){
     if(started==false){
         console.log("game started");
         started=true;
+        levelUp();
     }
-})
+});
+function btnFlash(btn){
+    btn.classList.add("flash");
+    setTimeout(function(){
+        btn.classList.remove("flash");
+    },250)
+
+}
+
+function levelUp(){
+    level++;
+    h2.innerText=`level ${level}`;
+
+    let randomIdx=Math.floor(Math.random()*3);
+    let randomColor=btns[randomIdx];
+    let randomBtn=document.querySelector(`.${randomColor}`)
+    console.log(randomIdx);
+     console.log(randomColor);
+     console.log(randomBtn);
+     btnFlash(randomBtn);
+        
+}
 
