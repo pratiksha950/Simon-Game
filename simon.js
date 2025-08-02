7c5890350cb20a891011197b9c2eb6cf99f31312
pratiksha950 <salunkepratiksha950@gmail.com>
2025-08-02 19:14:40 +0530

    clicking effect is applied

--- simon.js
+++ simon.js
@@ -13,15 +13,20 @@ document.addEventListener("keypress",function(){
         levelUp();
     }
 });
-function btnFlash(btn){
+function gameFlash(btn){
     btn.classList.add("flash");
     setTimeout(function(){
         btn.classList.remove("flash");
     },250)
-
 }
 
-function levelUp(){
+function userFlash(btn){
+    btn.classList.add("userFlash");
+    setTimeout(function(){
+        btn.classList.remove("userFlash");
+    },250)
+}
+function levelUp(){  
     level++;
     h2.innerText=`level ${level}`;
 
@@ -31,16 +36,16 @@ function levelUp(){
     console.log(randomIdx);
      console.log(randomColor);
      console.log(randomBtn);
-     btnFlash(randomBtn);
+     gameFlash(randomBtn);
         
 }
 function btnPress(){
     let btn=this;
-    btnFlash(btn);
+    userFlash(btn);
 
 }
 let allBtns=document.querySelectorAll(".btn");
-for(btn of allBtns){
+for(let btn of allBtns){
     btn.addEventListener("click",btnPress);
 }
 
